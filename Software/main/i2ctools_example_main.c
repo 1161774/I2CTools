f2cbe0ea91da0655d698e1292c52b4a011695a58
Benjamin Bowmer <benjamin.bowmer@gotosage.com>
2020-06-02 16:37:35 +0930

    Add software

---
+++ Software/main/i2ctools_example_main.c
@@ -0,0 +1,244 @@
+/* i2c-tools example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_console.h"
+#include "esp_vfs_dev.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "linenoise/linenoise.h"
+#include "argtable3/argtable3.h"
+#include "cmd_decl.h"
+#include "esp_vfs_fat.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "sdkconfig.h"
+
+static const char *TAG = "i2c-tools";
+
+#define GPIO_INPUT_I2CDetect     4
+
+static xQueueHandle gpio_evt_queue = NULL;
+
+#if CONFIG_EXAMPLE_STORE_HISTORY
+
+#define MOUNT_PATH "/data"
+#define HISTORY_PATH MOUNT_PATH "/history.txt"
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+	uint32_t gpio_num = (uint32_t) arg;
+	xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+	int ret;
+	uint32_t io_num;
+	for (;;) {
+		if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+			//printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+			esp_console_run("i2cdetect", &ret);
+		}
+	}
+}
+
+static void initialize_filesystem()
+{
+    static wl_handle_t wl_handle;
+    const esp_vfs_fat_mount_config_t mount_config = {
+        .max_files = 4,
+        .format_if_mount_failed = true
+    };
+    esp_err_t err = esp_vfs_fat_spiflash_mount(MOUNT_PATH, "storage", &mount_config, &wl_handle);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to mount FATFS (%s)", esp_err_to_name(err));
+        return;
+    }
+}
+#endif // CONFIG_EXAMPLE_STORE_HISTORY
+
+static void initialize_nvs()
+{
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+}
+
+static void initialise_GPIO()
+{
+	gpio_config_t io_conf;
+	
+	io_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
+	io_conf.pin_bit_mask = (1ULL << GPIO_INPUT_I2CDetect);
+	io_conf.mode = GPIO_MODE_INPUT;
+	io_conf.pull_down_en = 0;
+	io_conf.pull_up_en = 1;
+	
+	gpio_config(&io_conf);
+	
+	gpio_set_intr_type(GPIO_INPUT_I2CDetect, GPIO_INTR_POSEDGE);
+	
+	//create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+	//start gpio task
+	xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+	//install gpio isr service
+	gpio_install_isr_service(0);
+	//hook isr handler for specific gpio pin
+	gpio_isr_handler_add(GPIO_INPUT_I2CDetect, gpio_isr_handler, (void*) GPIO_INPUT_I2CDetect);
+	
+}
+
+static void initialize_console()
+{
+    /* Disable buffering on stdin */
+    setvbuf(stdin, NULL, _IONBF, 0);
+
+    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
+    esp_vfs_dev_uart_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+    /* Move the caret to the beginning of the next line on '\n' */
+    esp_vfs_dev_uart_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
+
+    /* Configure UART. Note that REF_TICK is used so that the baud rate remains
+     * correct while APB frequency is changing in light sleep mode.
+     */
+    const uart_config_t uart_config = {
+        .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .use_ref_tick = true
+    };
+    ESP_ERROR_CHECK(uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config));
+
+    /* Install UART driver for interrupt-driven reads and writes */
+    ESP_ERROR_CHECK(uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM,
+                                        256, 0, 0, NULL, 0));
+
+    /* Tell VFS to use UART driver */
+    esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
+
+    /* Initialize the console */
+    esp_console_config_t console_config = {
+        .max_cmdline_args = CONFIG_EXAMPLE_MAX_CMD_ARGUMENTS,
+        .max_cmdline_length = CONFIG_EXAMPLE_MAX_CMD_LENGTH,
+#if CONFIG_LOG_COLORS
+        .hint_color = atoi(LOG_COLOR_CYAN)
+#endif
+    };
+    ESP_ERROR_CHECK(esp_console_init(&console_config));
+
+    /* Configure linenoise line completion library */
+    linenoiseSetMultiLine(1);
+
+    /* Tell linenoise where to get command completions and hints */
+    linenoiseSetCompletionCallback(&esp_console_get_completion);
+    linenoiseSetHintsCallback((linenoiseHintsCallback *)&esp_console_get_hint);
+
+    /* Set command history size */
+    linenoiseHistorySetMaxLen(100);
+
+#if CONFIG_EXAMPLE_STORE_HISTORY
+    /* Load command history from filesystem */
+    linenoiseHistoryLoad(HISTORY_PATH);
+#endif
+}
+
+void app_main()
+{
+    initialize_nvs();
+
+#if CONFIG_EXAMPLE_STORE_HISTORY
+    initialize_filesystem();
+#endif
+
+    initialize_console();
+	
+	initialise_GPIO();
+
+    /* Register commands */
+    esp_console_register_help_command();
+    register_i2ctools();
+    register_system();
+
+    /* Prompt to be printed before each line.
+     * This can be customized, made dynamic, etc.
+     */
+    const char *prompt = LOG_COLOR_I "esp32> " LOG_RESET_COLOR;
+
+    printf("\n ==============================================================\n");
+    printf(" |       Steps to Use i2c-tools on ESP32                      |\n");
+    printf(" |                                                            |\n");
+    printf(" |  1. Try 'help', check all supported commands               |\n");
+    printf(" |  2. Try 'i2cconfig' to configure your I2C bus              |\n");
+    printf(" |  3. Try 'i2cdetect' to scan devices on the bus             |\n");
+    printf(" |  4. Try 'i2cget' to get the content of specific register   |\n");
+    printf(" |  5. Try 'i2cset' to set the value of specific register     |\n");
+    printf(" |  6. Try 'i2cdump' to dump all the register (Experiment)    |\n");
+    printf(" |                                                            |\n");
+    printf(" ==============================================================\n\n");
+
+    /* Figure out if the terminal supports escape sequences */
+    int probe_status = linenoiseProbe();
+    if (probe_status) {
+        /* zero indicates success */
+        printf("\n"
+               "Your terminal application does not support escape sequences.\n"
+               "Line editing and history features are disabled.\n"
+               "On Windows, try using Putty instead.\n");
+        linenoiseSetDumbMode(1);
+#if CONFIG_LOG_COLORS
+        /* Since the terminal doesn't support escape sequences,
+         * don't use color codes in the prompt.
+         */
+        prompt = "esp32> ";
+#endif //CONFIG_LOG_COLORS
+    }
+
+    /* Main loop */
+    while (true) {
+        /* Get a line using linenoise.
+         * The line is returned when ENTER is pressed.
+         */
+        char *line = linenoise(prompt);
+        if (line == NULL) {
+            /* Ignore empty lines */
+            continue;
+        }
+        /* Add the command to the history */
+        linenoiseHistoryAdd(line);
+#if CONFIG_EXAMPLE_STORE_HISTORY
+        /* Save command history to filesystem */
+        linenoiseHistorySave(HISTORY_PATH);
+#endif
+
+        /* Try to run the command */
+        int ret;
+        esp_err_t err = esp_console_run(line, &ret);
+        if (err == ESP_ERR_NOT_FOUND) {
+            printf("Unrecognized command\n");
+        } else if (err == ESP_ERR_INVALID_ARG) {
+            // command was empty
+        } else if (err == ESP_OK && ret != ESP_OK) {
+            printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(err));
+        } else if (err != ESP_OK) {
+            printf("Internal error: %s\n", esp_err_to_name(err));
+        }
+        /* linenoise allocates line buffer on the heap, so need to free it */
+        linenoiseFree(line);
+    }
+}
